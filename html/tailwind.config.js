c9067707fc61854323c70e7a57c30f1ac279cfdb
BosSupaschai Ph <112678332+SupaschaiPh@users.noreply.github.com>
2023-10-13 17:43:38 +0700

    add css

--- html/tailwind.config.js
+++ html/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				primary: {
+					50: '#edfcf3',
+					100: '#d3f8e0',
+					200: '#abefc8',
+					300: '#74e1a9',
+					400: '#52d193',
+					500: '#18b16b',
+					600: '#0c8f56',
+					700: '#0a7248',
+					800: '#0a5b3a',
+					900: '#0a4a31',
+					950: '#042a1c'
+				}
+			}
+		}
   },
   plugins: [require('flowbite/plugin')],
 }
